9f7c7bedd6f3db0754ef36d25abd5dbd5156e637
Dummy <onyourleftpal@gmail.com>
2024-11-07 20:06:51 +0300

    telegram capture photo issue fixed

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.DB_STRING);
+        await mongoose.connect(process.env.DB_STRING_LOCAL);
         console.log('MongoDB connected');
     } catch (err) {
         console.error(err.message);
